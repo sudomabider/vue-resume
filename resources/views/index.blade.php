365c38a0722adebc7759450487b7df591bf01523
vagrant <veoc.dev@gmail.com>
2016-12-30 21:49:59 +0000

    minify

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,5 @@
 <div id="root"></div>
 
 <script src="{{ elixir('js/app.js') }}"></script>
-<script
-        src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous"></script>
 </body>
 </html>
