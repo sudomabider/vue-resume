2f2a545d4bae08adc76271338437b2485611a98a
vagrant <veoc.dev@gmail.com>
2016-12-30 19:35:03 +0000

    remove auth links

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -8,16 +8,16 @@
                 </keep-alive>
             </transition>
         </main>
-        <footer>
-            <ul class="nav nav-pills">
-                <li class="" v-if="!user.authenticated">
-                    <router-link :to="{ name: 'login' }">Login</router-link>
-                </li>
-                <li class="" v-if="user.authenticated">
-                    <router-link to="" @click.prevent="logout">Logout</router-link>
-                </li>
-            </ul>
-        </footer>
+        <!--<footer>-->
+            <!--<ul class="nav nav-pills">-->
+                <!--<li class="" v-if="!user.authenticated">-->
+                    <!--<router-link :to="{ name: 'login' }">Login</router-link>-->
+                <!--</li>-->
+                <!--<li class="" v-if="user.authenticated">-->
+                    <!--<router-link to="" @click.prevent="logout">Logout</router-link>-->
+                <!--</li>-->
+            <!--</ul>-->
+        <!--</footer>-->
     </div>
 </template>
 
